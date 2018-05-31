34b95555be1581508e48f1156a21f895a2a32ce7
Teodor <thelad43@gmail.com>
2018-05-31 13:51:10 +0300

    Added solution for problem 02. Bank Account Methods

---
+++ 01. Defining Classes - Lab/02. Bank Account Methods/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿namespace _02._Bank_Account_Methods
+{
+    using System;
+
+    public class BankAccount
+    {
+        public BankAccount(int id)
+        {
+            this.Id = id;
+        }
+
+        public int Id { get; private set; }
+
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (this.Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient balance");
+            }
+
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account {this.Id}, balance {this.Balance}";
+        }
+    }
+}
---
+++ 01. Defining Classes - Lab/02. Bank Account Methods/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace _02._Bank_Account_Methods
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var acc = new BankAccount(1);
+
+            acc.Deposit(50);
+            acc.Withdraw(20);
+
+            Console.WriteLine(acc);
+        }
+    }
+}
