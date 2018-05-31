9529a01ee2e5235a2b3a9391a6dc326ff960a46e
Teodor <thelad43@gmail.com>
2018-05-31 13:49:09 +0300

    Added solution for problem 01. Bank Account

---
+++ 01. Defining Classes - Lab/01. Bank Account/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿namespace _01._Bank_Account
+{
+    public class BankAccount
+    {
+        public BankAccount(int id, decimal balance)
+        {
+            this.Id = id;
+            this.Balance = balance;
+        }
+
+        public int Id { get; private set; }
+
+        public decimal Balance { get; private set; }
+    }
+}
---
+++ 01. Defining Classes - Lab/01. Bank Account/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace _01._Bank_Account
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var acc = new BankAccount(1, 50);
+
+            Console.WriteLine($"Account {acc.Id}, balance {acc.Balance}");
+        }
+    }
+}
