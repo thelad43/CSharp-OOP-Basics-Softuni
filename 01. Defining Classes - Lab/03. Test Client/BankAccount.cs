5b61072380c3f273dfd1139555845157ae0b0e13
Teodor <thelad43@gmail.com>
2018-05-31 13:56:26 +0300

    Added solution for problem 03. Test Client

---
+++ 01. Defining Classes - Lab/03. Test Client/BankAccount.cs
@@ -0,0 +1,40 @@
+﻿namespace _03._Test_Client
+{
+    using System;
+
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        public BankAccount(int id)
+        {
+            this.Id = id;
+        }
+
+        public int Id { get; private set; }
+
+        public decimal Balance { get; private set; }
+
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (this.Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient balance");
+            }
+
+            this.Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Account ID{this.Id}, balance {this.Balance:F2}";
+        }
+    }
+}
---
+++ 01. Defining Classes - Lab/03. Test Client/StartUp.cs
@@ -0,0 +1,112 @@
+﻿namespace _03._Test_Client
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        private static List<BankAccount> bankAccounts;
+
+        public static void Main()
+        {
+            bankAccounts = new List<BankAccount>();
+
+            while (true)
+            {
+                var line = Console.ReadLine().Split();
+
+                if (line[0] == "End")
+                {
+                    break;
+                }
+
+                try
+                {
+                    // switch on command
+                    switch (line[0])
+                    {
+                        case "Create":
+                            CreateAccount(int.Parse(line[1]));
+                            break;
+
+                        case "Deposit":
+                            Deposit(int.Parse(line[1]), decimal.Parse(line[2]));
+                            break;
+
+                        case "Withdraw":
+                            Withdraw(int.Parse(line[1]), decimal.Parse(line[2]));
+                            break;
+
+                        case "Print":
+                            Print(int.Parse(line[1]));
+                            break;
+
+                        default:
+                            throw new ArgumentException();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void Print(int id)
+        {
+            if (!AccountExists(id))
+            {
+                throw new InvalidOperationException("Account does not exist");
+            }
+
+            var acc = bankAccounts.Single(ba => ba.Id == id);
+            Console.WriteLine(acc);
+        }
+
+        private static void Withdraw(int id, decimal amount)
+        {
+            if (!AccountExists(id))
+            {
+                throw new InvalidOperationException("Account does not exist");
+            }
+
+            var acc = bankAccounts.Single(ba => ba.Id == id);
+            acc.Withdraw(amount);
+        }
+
+        private static void Deposit(int id, decimal amount)
+        {
+            if (!AccountExists(id))
+            {
+                throw new InvalidOperationException("Account does not exist");
+            }
+
+            var acc = bankAccounts.Single(ba => ba.Id == id);
+            acc.Deposit(amount);
+        }
+
+        private static void CreateAccount(int id)
+        {
+            var acc = bankAccounts.SingleOrDefault(ba => ba.Id == id);
+
+            if (acc == null)
+            {
+                bankAccounts.Add(new BankAccount(id));
+            }
+            else
+            {
+                throw new InvalidOperationException("Account already exists");
+            }
+        }
+
+        private static bool AccountExists(int id)
+        {
+            var acc = new BankAccount(id);
+
+            var exists = bankAccounts.Any(ba => ba.Id == acc.Id);
+
+            return exists;
+        }
+    }
+}
