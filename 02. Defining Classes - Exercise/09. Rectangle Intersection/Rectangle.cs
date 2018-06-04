3d1ba7bb3094c934cf89023817d9dd7d8b1901a4
Teodor <thelad43@gmail.com>
2018-06-04 17:26:22 +0300

    Added solution for problem 09. Rectangle Intersection

---
+++ 02. Defining Classes - Exercise/09. Rectangle Intersection/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿namespace _09._Rectangle_Intersection
+{
+    public class Rectangle
+    {
+        public Rectangle()
+        {
+        }
+
+        public Rectangle(string id, double width, double height, double topLeftX, double topLeftY)
+        {
+            this.Id = id;
+            this.Width = width;
+            this.Height = height;
+            this.TopLeftX = topLeftX;
+            this.TopLeftY = topLeftY;
+        }
+
+        public string Id { get; set; }
+
+        public double Width { get; set; }
+
+        public double Height { get; set; }
+
+        public double TopLeftX { get; set; }
+
+        public double TopLeftY { get; set; }
+
+        public bool HasIntersect(Rectangle secondRectangle)
+        {
+            return secondRectangle.TopLeftX + secondRectangle.Width >= this.TopLeftX &&
+                secondRectangle.TopLeftX <= this.TopLeftX + this.Width &&
+                secondRectangle.TopLeftY >= this.TopLeftY - this.Height &&
+                secondRectangle.TopLeftY - secondRectangle.Height <= this.TopLeftY;
+        }
+    }
+}
---
+++ 02. Defining Classes - Exercise/09. Rectangle Intersection/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace _09._Rectangle_Intersection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputTokens = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var rectangles = SetRectangles(inputTokens[0]);
+
+            CheckIntersections(inputTokens[1], rectangles);
+        }
+
+        private static void CheckIntersections(int numberOfIntersectionChecks, Queue<Rectangle> rectangles)
+        {
+            while (numberOfIntersectionChecks > 0)
+            {
+                var pair = Console.ReadLine().Split();
+                var firstRect = rectangles.Where(r => r.Id == pair[0]).FirstOrDefault();
+                var secondRect = rectangles.Where(r => r.Id == pair[1]).FirstOrDefault();
+                Console.WriteLine(firstRect.HasIntersect(secondRect) ? "true" : "false");
+                numberOfIntersectionChecks--;
+            }
+        }
+
+        private static Queue<Rectangle> SetRectangles(int numberOfRectangles)
+        {
+            var rectangles = new Queue<Rectangle>(numberOfRectangles);
+
+            while (rectangles.Count < numberOfRectangles)
+            {
+                var input = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                rectangles.Enqueue(new Rectangle(input[0], double.Parse(input[1]),
+                    double.Parse(input[2]), double.Parse(input[3]), double.Parse(input[4])));
+            }
+
+            return rectangles;
+        }
+    }
+}
