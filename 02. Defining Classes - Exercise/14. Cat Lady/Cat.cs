0d39aa9e649c85cf86b38f3b9b187d75d6429061
Teodor <thelad43@gmail.com>
2018-06-04 17:34:06 +0300

    Added solution for problem 14. Cat Lady

---
+++ 02. Defining Classes - Exercise/14. Cat Lady/Cat.cs
@@ -0,0 +1,42 @@
+﻿namespace _14._Cat_Lady
+{
+    public class Cat
+    {
+        public Cat()
+        {
+        }
+
+        public Cat(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public string Breed { get; set; }
+
+        public int EarSize { get; set; }
+
+        public int DecibelsOfMeows { get; set; }
+
+        public double FurLength { get; set; }
+
+        public override string ToString()
+        {
+            if (this.Breed == "Siamese")
+            {
+                return $"{this.Breed} {this.Name} {this.EarSize}";
+            }
+            else if (this.Breed == "Cymric")
+            {
+                return $"{this.Breed} {this.Name} {this.FurLength:F2}";
+            }
+            else if (this.Breed == "StreetExtraordinaire")
+            {
+                return $"{this.Breed} {this.Name} {this.DecibelsOfMeows}";
+            }
+
+            return null;
+        }
+    }
+}
---
+++ 02. Defining Classes - Exercise/14. Cat Lady/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace _14._Cat_Lady
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var cats = ReadCats();
+            PrintCat(cats);
+        }
+
+        private static void PrintCat(List<Cat> cats)
+        {
+            var catName = Console.ReadLine();
+            var catToPrint = cats.FirstOrDefault(c => c.Name == catName);
+            Console.WriteLine(catToPrint);
+        }
+
+        private static List<Cat> ReadCats()
+        {
+            var cats = new List<Cat>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (inputLine == "End")
+                {
+                    break;
+                }
+
+                var catInfo = inputLine
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var cat = new Cat
+                {
+                    Breed = catInfo[0],
+                    Name = catInfo[1]
+                };
+
+                if (cat.Breed == "Siamese")
+                {
+                    cat.EarSize = int.Parse(catInfo[2]);
+                }
+                else if (cat.Breed == "Cymric")
+                {
+                    cat.FurLength = double.Parse(catInfo[2]);
+                }
+                else if (cat.Breed == "StreetExtraordinaire")
+                {
+                    cat.DecibelsOfMeows = int.Parse(catInfo[2]);
+                }
+
+                cats.Add(cat);
+            }
+
+            return cats;
+        }
+    }
+}
