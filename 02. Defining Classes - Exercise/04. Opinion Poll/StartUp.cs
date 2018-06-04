5fe9ba5fa9e1987b6ef9004e26386ddd8a1e87f3
Teodor <thelad43@gmail.com>
2018-06-04 17:12:59 +0300

    Added solution for problem 04. Opinion Poll

---
+++ 02. Defining Classes - Exercise/04. Opinion Poll/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace _04._Opinion_Poll
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        private static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var people = new List<Person>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                // {name} {age}
+                var tokens = Console.ReadLine().Split();
+                people.Add(new Person(tokens[0], int.Parse(tokens[1])));
+            }
+
+            people
+                .Where(p => p.Age > 30)
+                .OrderBy(p => p.Name)
+                .Select(p => $"{p.Name} - {p.Age}")
+                .ToList()
+                .ForEach(p => Console.WriteLine(p));
+        }
+    }
+}
