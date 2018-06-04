6b37d359c57677a0d77451c104868d47b16864c9
Teodor <thelad43@gmail.com>
2018-06-04 17:19:37 +0300

    Added solution for problem 05. Date Modifier

---
+++ 02. Defining Classes - Exercise/05. Date Modifier/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿namespace _05._Date_Modifier
+{
+    using System;
+    using System.Linq;
+
+    public class DateModifier
+    {
+        public DateTime FirstDateTime { get; set; }
+
+        public DateTime SecondDateTime { get; set; }
+
+        public static int CalculateDifference(string firstDateAsString, string secondDateAsString)
+        {
+            var firstDateTokens = firstDateAsString
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var secondDateTokens = secondDateAsString
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var firstDate = new DateTime(firstDateTokens[0], firstDateTokens[1], firstDateTokens[2]);
+            var secondDate = new DateTime(secondDateTokens[0], secondDateTokens[1], secondDateTokens[2]);
+
+            var differenceDays = (firstDate - secondDate).Days;
+            return differenceDays;
+        }
+    }
+}
---
+++ 02. Defining Classes - Exercise/05. Date Modifier/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _05._Date_Modifier
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var firstDate = Console.ReadLine();
+            var secondDate = Console.ReadLine();
+
+            var diff = DateModifier.CalculateDifference(firstDate, secondDate);
+            Console.WriteLine(Math.Abs(diff));
+        }
+    }
+}
