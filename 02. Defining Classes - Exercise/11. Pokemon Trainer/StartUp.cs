8d70e04b730f473e66da1d334655e5ba5b0087d8
Teodor <thelad43@gmail.com>
2018-06-04 17:29:13 +0300

    Added solution for problem 11. Pokemon Trainer

---
+++ 02. Defining Classes - Exercise/11. Pokemon Trainer/StartUp.cs
@@ -0,0 +1,103 @@
+﻿namespace _11._Pokemon_Trainer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var trainers = ReadTrainers();
+
+            ProcessElements(trainers);
+
+            PrintTrainers(trainers);
+        }
+
+        private static void PrintTrainers(Queue<Trainer> trainers)
+        {
+            var result = trainers
+                .OrderByDescending(t => t.NumberOfBadges)
+                .Select(t => $"{t.Name} {t.NumberOfBadges} {t.Pokemons.Count}")
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void ProcessElements(Queue<Trainer> trainers)
+        {
+            while (true)
+            {
+                var element = Console.ReadLine();
+
+                if (element == "End")
+                {
+                    break;
+                }
+
+                foreach (var trainer in trainers)
+                {
+                    if (trainer.Pokemons.Where(p => p.Element == element).FirstOrDefault() == null)
+                    {
+                        foreach (var pokemon in trainer.Pokemons)
+                        {
+                            pokemon.ReduceHealth();
+                        }
+
+                        trainer.ClearDeadPokemons();
+                    }
+                    else
+                    {
+                        trainer.NumberOfBadges++;
+                    }
+                }
+            }
+        }
+
+        private static Queue<Trainer> ReadTrainers()
+        {
+            var trainers = new Queue<Trainer>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (inputLine == "Tournament")
+                {
+                    break;
+                }
+
+                // <TrainerName> <PokemonName> <PokemonElement> <PokemonHealth>
+                var pokemonAndTrainerInfo = inputLine
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                var trainerName = pokemonAndTrainerInfo[0];
+                var pokemonName = pokemonAndTrainerInfo[1];
+                var pokemonElement = pokemonAndTrainerInfo[2];
+                var pokemonHealth = int.Parse(pokemonAndTrainerInfo[3]);
+
+                var pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                var trainer = trainers.Where(t => t.Name == trainerName)
+                    .FirstOrDefault();
+
+                if (trainer == null)
+                {
+                    trainer = new Trainer(trainerName);
+                }
+
+                if (!trainers.Contains(trainer))
+                {
+                    trainers.Enqueue(trainer);
+                }
+
+                trainer.Pokemons.Add(pokemon);
+            }
+
+            return trainers;
+        }
+    }
+}
---
+++ 02. Defining Classes - Exercise/11. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,33 @@
+﻿namespace _11._Pokemon_Trainer
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Trainer
+    {
+        public Trainer()
+        {
+        }
+
+        public Trainer(string name, int numberOfBadges = 0)
+        {
+            this.Name = name;
+            this.NumberOfBadges = numberOfBadges;
+            this.Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+        internal void ClearDeadPokemons()
+        {
+            if (this.Pokemons.Count > 0 && this.Pokemons.Where(p => p.Health <= 0).FirstOrDefault() != null)
+            {
+                this.Pokemons = new List<Pokemon>(this.Pokemons.Where(p => p.Health > 0));
+            }
+        }
+    }
+}
