053c2582fe085b468dcbf52f25dc47146f38960b
Teodor <thelad43@gmail.com>
2018-06-04 17:05:47 +0300

    Added solution for problem 01. Define a Class Person

---
+++ 02. Defining Classes - Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace _01._Define_a_Class_Person
+{
+    public class Person
+    {
+        public Person()
+        {
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
---
+++ 02. Defining Classes - Exercise/01. Define a Class Person/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Define_a_Class_Person
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var p1 = new Person("Pesho", 20);
+            var p2 = new Person("Gosho", 18);
+            var p3 = new Person("Stamat", 43);
+        }
+    }
+}
