70c3515bac84680ca5b47e4d07904b0b436b6a86
Teodor <thelad43@gmail.com>
2018-06-06 22:42:03 +0300

    Added solution for problem 02. Car Salesman

---
+++ 04. Working with Abstraction - Exercise/02. Car Salesman/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace _02._Car_Salesman
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+        {
+            this.Model = model;
+            this.Engine = engine;
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public string Model { get; set; }
+
+        public Engine Engine { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Color { get; set; }
+
+        public override string ToString()
+        {
+            return $"Car: {this.Model}, Engine: {this.Engine}, Weight: {this.Weight}, Color: {this.Color}";
+        }
+    }
+}
