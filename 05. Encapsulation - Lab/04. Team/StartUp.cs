89ddb7561b61dc69e25100a4fbc95cc1e7a9dedd
Teodor <thelad43@gmail.com>
2018-06-07 19:41:28 +0300

    Added solution for problem 04. Team

---
+++ 05. Encapsulation - Lab/04. Team/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace _04._Team
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var team = new Team("Levski");
+
+            for (int i = 0; i < n; i++)
+            {
+                var personInfo = Console.ReadLine().Split();
+                var person = new Person(personInfo[0], personInfo[1], int.Parse(personInfo[2]), decimal.Parse(personInfo[3]));
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
---
+++ 05. Encapsulation - Lab/04. Team/Team.cs
@@ -0,0 +1,30 @@
+﻿namespace _04._Team
+{
+    using System.Collections.Generic;
+
+    public class Team
+    {
+        public Team(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public List<Person> FirstTeam { get; } = new List<Person>();
+
+        public List<Person> ReserveTeam { get; } = new List<Person>();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                this.FirstTeam.Add(person);
+            }
+            else
+            {
+                this.ReserveTeam.Add(person);
+            }
+        }
+    }
+}
