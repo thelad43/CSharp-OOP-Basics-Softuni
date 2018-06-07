90b98ffeb33a5d7142f3183a0662745b854ff995
Teodor <thelad43@gmail.com>
2018-06-07 19:37:29 +0300

    Added solution for problem 01. Persons

---
+++ 05. Encapsulation - Lab/01. Persons/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Persons
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var person = new Person(cmdArgs[0], cmdArgs[1], int.Parse(cmdArgs[2]));
+                persons.Add(person);
+            }
+
+            persons.OrderBy(p => p.FirstName)
+                .ThenBy(p => p.Age)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
