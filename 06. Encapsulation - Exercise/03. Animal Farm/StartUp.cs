56bfb37e3d1f653d66d2c49b20d183dcaf27969f
Teodor <thelad43@gmail.com>
2018-06-10 11:37:02 +0300

    Added solution for problem 03. Animal Farm

---
+++ 06. Encapsulation - Exercise/03. Animal Farm/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _03._Animal_Farm
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+
+            Chicken chicken = null;
+
+            try
+            {
+                chicken = new Chicken(name, age);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+                return;
+            }
+
+            Console.WriteLine($"Chicken {chicken.Name} (age {chicken.Age}) can produce {chicken.ProductPerDay} eggs per day.");
+        }
+    }
+}
