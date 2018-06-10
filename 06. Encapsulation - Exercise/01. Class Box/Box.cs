c71b05fde60fc646ca43fd4ada6f5ee324800847
Teodor <thelad43@gmail.com>
2018-06-10 11:34:23 +0300

    Added solution for problem 01. Class Box

---
+++ 06. Encapsulation - Exercise/01. Class Box/Box.cs
@@ -0,0 +1,70 @@
+﻿namespace _01._Class_Box
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return this.width;
+            }
+            set
+            {
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return this.height;
+            }
+            set
+            {
+                this.height = value;
+            }
+        }
+
+        public double CalculateSurfaceArea()
+        {
+            var surfaceArea = 2 * this.Length * this.Width + 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+            return surfaceArea;
+        }
+
+        public double CalculateLateralSurfaceArea()
+        {
+            var lateralSurfaceArea = 2 * this.Length * this.Height + 2 * this.Width * this.Height;
+            return lateralSurfaceArea;
+        }
+
+        public double CalculateVolume()
+        {
+            var volume = this.Length * this.Height * this.Width;
+            return volume;
+        }
+    }
+}
---
+++ 06. Encapsulation - Exercise/01. Class Box/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Class_Box
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            var box = new Box(length, width, height);
+
+            var surfArea = box.CalculateSurfaceArea();
+            var latSurfArea = box.CalculateLateralSurfaceArea();
+            var volume = box.CalculateVolume();
+
+            Console.WriteLine($"Surface Area - {surfArea:F2}");
+            Console.WriteLine($"Lateral Surface Area - {latSurfArea:F2}");
+            Console.WriteLine($"Volume - {volume:F2}");
+        }
+    }
+}
