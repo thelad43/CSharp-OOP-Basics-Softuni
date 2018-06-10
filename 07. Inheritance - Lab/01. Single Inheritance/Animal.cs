9aea36b8a00c7b200a48b32f2222f8c5afa74468
Teodor <thelad43@gmail.com>
2018-06-10 17:48:35 +0300

    Added solution for problem 01. Single Inheritance

---
+++ 07. Inheritance - Lab/01. Single Inheritance/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Single_Inheritance
+{
+    using System;
+
+    public abstract class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ 07. Inheritance - Lab/01. Single Inheritance/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace _01._Single_Inheritance
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
