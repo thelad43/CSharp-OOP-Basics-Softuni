57f8e8499196562e450a5f4aee1f75beb3af85d7
Teodor <thelad43@gmail.com>
2018-06-10 17:54:35 +0300

    Added solution for problem 05. Stack of Strings

---
+++ 07. Inheritance - Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,65 @@
+﻿namespace _05._Stack_of_Strings
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StackOfStrings : IEnumerable
+    {
+        private List<string> data = new List<string>();
+
+        public void Push(string item)
+        {
+            this.data.Add(item);
+        }
+
+        public string Pop()
+        {
+            var lastItem = this.data.LastOrDefault();
+
+            if (lastItem == null)
+            {
+                throw new InvalidOperationException("The StackOfStrings is empty. Cannot pop any element!");
+            }
+
+            this.data.Remove(lastItem);
+            return lastItem;
+        }
+
+        public string Peek()
+        {
+            var lastItem = this.data.LastOrDefault();
+
+            if (lastItem == null)
+            {
+                throw new InvalidOperationException("The StackOfStrings is empty. Cannot pop any element!");
+            }
+
+            return lastItem;
+        }
+
+        public bool IsEmpty()
+        {
+            if (this.data.Any())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            foreach (var item in this.data)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ 07. Inheritance - Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,61 @@
+﻿namespace _05._Stack_of_Strings
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var stackOfStrings = new StackOfStrings();
+
+            stackOfStrings.Push("aaa");
+            stackOfStrings.Push("bbb");
+            stackOfStrings.Push("ccc");
+            stackOfStrings.Push("ddd");
+            stackOfStrings.Push("eee");
+            stackOfStrings.Push("fff");
+
+            Console.Write("Elements in the StackOfStrings: ");
+            PrintStackOfStrings(stackOfStrings);
+
+            var removed = stackOfStrings.Pop();
+            Console.WriteLine($"After removing element {removed}");
+            PrintStackOfStrings(stackOfStrings);
+
+            removed = stackOfStrings.Pop();
+            Console.WriteLine($"After removing element: {removed}");
+            PrintStackOfStrings(stackOfStrings);
+
+            var peekElement = stackOfStrings.Peek();
+            Console.WriteLine($"Peeked element: {peekElement}");
+            Console.Write("Elements in the StackOfStrings: ");
+            PrintStackOfStrings(stackOfStrings);
+
+            stackOfStrings.Pop();
+            stackOfStrings.Pop();
+            stackOfStrings.Pop();
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            PrintStackOfStrings(stackOfStrings);
+
+            stackOfStrings.Pop();
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            PrintStackOfStrings(stackOfStrings);
+        }
+
+        private static void PrintStackOfStrings(StackOfStrings stackOfStrings)
+        {
+            var stringBuilder = new StringBuilder();
+
+            foreach (var element in stackOfStrings)
+            {
+                stringBuilder.Append($"{element} ");
+            }
+
+            stringBuilder.ToString().Trim();
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
