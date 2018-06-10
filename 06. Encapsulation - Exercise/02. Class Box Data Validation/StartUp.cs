e774b249515c3dacc370fb58e5be347e93b4e917
Teodor <thelad43@gmail.com>
2018-06-10 11:35:32 +0300

    Added solution for problem 02. Class Box Data Validation

---
+++ 06. Encapsulation - Exercise/02. Class Box Data Validation/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._Class_Box_Data_Validation
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var length = double.Parse(Console.ReadLine());
+            var width = double.Parse(Console.ReadLine());
+            var height = double.Parse(Console.ReadLine());
+
+            Box box = null;
+
+            try
+            {
+                box = new Box(length, width, height);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+                return;
+            }
+
+            var surfArea = box.CalculateSurfaceArea();
+            var latSurfArea = box.CalculateLateralSurfaceArea();
+            var volume = box.CalculateVolume();
+
+            Console.WriteLine($"Surface Area - {surfArea:F2}");
+            Console.WriteLine($"Lateral Surface Area - {latSurfArea:F2}");
+            Console.WriteLine($"Volume - {volume:F2}");
+        }
+    }
+}
