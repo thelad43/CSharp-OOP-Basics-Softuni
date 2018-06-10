7ad987cd3b9fe14390228cb5aa0dcf89664738cf
Teodor <thelad43@gmail.com>
2018-06-10 17:52:51 +0300

    Added solution for problem 04. Random List

---
+++ 07. Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,18 @@
+﻿namespace _04._Random_List
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class RandomList : List<string>
+    {
+        private Random random = new Random();
+
+        public string RandomString()
+        {
+            var randomIndex = this.random.Next(0, this.Count - 1);
+            var removedItem = this[randomIndex];
+            this.RemoveAt(randomIndex);
+            return removedItem;
+        }
+    }
+}
---
+++ 07. Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _04._Random_List
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var words = new RandomList
+            {
+                "aaa",
+                "bbb",
+                "ccc",
+                "ddd",
+                "eee",
+                "iii",
+                "kkk",
+                "qqq"
+            };
+
+            var removed = words.RandomString();
+
+            Console.Write("Removed Item: ");
+            Console.WriteLine(removed);
+
+            Console.WriteLine();
+            Console.WriteLine(new string('-', 15));
+            Console.WriteLine();
+
+            Console.Write("Left Items: ");
+            Console.WriteLine(string.Join(" ", words));
+        }
+    }
+}
