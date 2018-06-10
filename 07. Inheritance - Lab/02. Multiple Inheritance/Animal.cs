cb777688bce5e7e6b0a8f5a99acf9866cdcfda54
Teodor <thelad43@gmail.com>
2018-06-10 17:49:59 +0300

    Added solution for problem 02. Multiple Inheritance

---
+++ 07. Inheritance - Lab/02. Multiple Inheritance/Animal.cs
@@ -0,0 +1,12 @@
+﻿namespace _02._Multiple_Inheritance
+{
+    using System;
+
+    public abstract class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ 07. Inheritance - Lab/02. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace _02._Multiple_Inheritance
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
