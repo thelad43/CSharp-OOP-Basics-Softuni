cb7e7735f1a2ae175243b98470b322b82a969643
Teodor <thelad43@gmail.com>
2018-06-12 15:44:29 +0300

    Added solution for problem 01. Person

---
+++ 08. Inheritance - Exercise/01. Person/Child.cs
@@ -0,0 +1,30 @@
+﻿namespace _01._Person
+{
+    using System;
+
+    public class Child : Person
+    {
+        public Child(string name, int age)
+            : base(name, age)
+        {
+            this.Age = age;
+        }
+
+        public new int Age
+        {
+            get
+            {
+                return base.Age;
+            }
+            set
+            {
+                if (value > 14)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ 08. Inheritance - Exercise/01. Person/Person.cs
@@ -0,0 +1,60 @@
+﻿namespace _01._Person
+{
+    using System;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Name's length should not be less than 3 symbols!");
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder.Append(String.Format($"Name: {this.Name}, Age: {this.Age}"));
+
+            return stringBuilder.ToString();
+        }
+    }
+}
