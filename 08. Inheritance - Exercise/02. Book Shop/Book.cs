26a9abc3d6eec59d10df2ff3c7ba0157929adbc6
Teodor <thelad43@gmail.com>
2018-06-12 15:47:06 +0300

    Added solution for problem 02. Book Shop

---
+++ 08. Inheritance - Exercise/02. Book Shop/Book.cs
@@ -0,0 +1,86 @@
+﻿namespace _02._Book_Shop
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class Book
+    {
+        private string title;
+        private string author;
+        private decimal price;
+
+        public Book(string author, string title, decimal price)
+        {
+            this.Author = author;
+            this.Title = title;
+            this.Price = price;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Title not valid!");
+                }
+
+                this.title = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return this.author;
+            }
+            set
+            {
+                var auhtorSecondName = value.Split(' ').LastOrDefault().ToString();
+
+                if (char.IsDigit(auhtorSecondName[0]))
+                {
+                    throw new ArgumentException("Author not valid!");
+                }
+
+                this.author = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Price not valid!");
+                }
+
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var resultBuilder = new StringBuilder();
+
+            resultBuilder.AppendLine($"Type: {this.GetType().Name}")
+                .AppendLine($"Title: {this.Title}")
+                .AppendLine($"Author: {this.Author}")
+                .AppendLine($"Price: {this.Price:f2}");
+
+            var result = resultBuilder.ToString().TrimEnd();
+            return result;
+        }
+    }
+}
---
+++ 08. Inheritance - Exercise/02. Book Shop/GoldenEditionBook.cs
@@ -0,0 +1,26 @@
+﻿namespace _02._Book_Shop
+{
+    public class GoldenEditionBook : Book
+    {
+        private const decimal PercantagesOfPriceGoldenBook = 0.3M;
+
+        public GoldenEditionBook(string title, string author, decimal price)
+            : base(title, author, price)
+        {
+            this.Price = price;
+        }
+
+        public new decimal Price
+        {
+            get
+            {
+                return base.Price;
+            }
+            set
+            {
+                var price = value + (value * PercantagesOfPriceGoldenBook);
+                base.Price = price;
+            }
+        }
+    }
+}
