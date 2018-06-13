77d709800065b5778271b4bbec939f1ee9d0786e
Teodor <thelad43@gmail.com>
2018-06-13 19:20:15 +0300

    Added solution for problem 01. Shapes

---
+++ 09. Interfaces and Abstraction - Lab/01. Shapes/Rectangle.cs
@@ -0,0 +1,74 @@
+﻿namespace _01._Shapes
+{
+    using System;
+
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Width
+        {
+            get
+            {
+                return this.width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be less than 1");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return this.height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be less than 1");
+                }
+
+                this.height = value;
+            }
+        }
+
+        private void DrawLine(int width, char end, char middle)
+        {
+            Console.Write(end);
+
+            for (int i = 1; i < width - 1; i++)
+            {
+                Console.Write(middle);
+            }
+
+            Console.WriteLine(end);
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+
+            for (int i = 1; i < this.Height - 1; i++)
+            {
+                DrawLine(this.Width, '*', ' ');
+            }
+
+            DrawLine(this.Width, '*', '*');
+        }
+    }
+}
---
+++ 09. Interfaces and Abstraction - Lab/01. Shapes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Shapes
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var radius = int.Parse(Console.ReadLine());
+            var circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            var rectangle = new Rectangle(width, height);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
