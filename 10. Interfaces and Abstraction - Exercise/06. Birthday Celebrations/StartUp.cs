bce15f2fb75c3ed9f625ab35a57f46e6a6713640
Teodor <thelad43@gmail.com>
2018-06-14 21:41:24 +0300

    Added solution for problem 06. Birthday Celebrations

---
+++ 10. Interfaces and Abstraction - Exercise/06. Birthday Celebrations/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace _06._Birthday_Celebrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var allYears = new Queue<string>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "End")
+                {
+                    break;
+                }
+
+                var lineTokens = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (!lineTokens[0].StartsWith("Robot"))
+                {
+                    var year = lineTokens.LastOrDefault();
+                    allYears.Enqueue(year);
+                }
+            }
+
+            var wantedYear = Console.ReadLine();
+
+            var result = allYears
+                .Where(y => y.EndsWith(wantedYear))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
