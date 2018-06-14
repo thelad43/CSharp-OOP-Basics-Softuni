de675d6ddbc6dd62f801fca2ae1f393507530cd2
Teodor <thelad43@gmail.com>
2018-06-14 21:33:18 +0300

    Added solution for problem 01. Define an Interface IPerson

---
+++ 10. Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -0,0 +1,15 @@
+﻿namespace _01._Define_an_Interface_IPerson
+{
+    public class Citizen : IPerson
+    {
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+    }
+}
---
+++ 10. Interfaces and Abstraction - Exercise/01. Define an Interface IPerson/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _01._Define_an_Interface_IPerson
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var person = new Citizen(name, age);
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
