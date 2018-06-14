54b13427ad58a2c4657ee5ef060ce08c58cef8c3
Teodor <thelad43@gmail.com>
2018-06-14 21:40:37 +0300

    Added solution for problem 05. Border Control

---
+++ 10. Interfaces and Abstraction - Exercise/05. Border Control/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _05._Border_Control
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var allIds = new HashSet<string>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "End")
+                {
+                    break;
+                }
+
+                var lineTokens = line
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var id = lineTokens.LastOrDefault();
+                allIds.Add(id);
+            }
+
+            var endingPartOfFakeId = Console.ReadLine();
+
+            var detainedIds = allIds
+                .Where(id => id.EndsWith(endingPartOfFakeId))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, detainedIds));
+        }
+    }
+}
