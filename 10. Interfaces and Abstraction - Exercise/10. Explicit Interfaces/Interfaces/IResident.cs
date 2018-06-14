58b53c7025ad58749c978acfe31e10a225123800
Teodor <thelad43@gmail.com>
2018-06-14 21:58:34 +0300

    Added solution for problem 10. Explicit Interfaces

---
+++ 10. Interfaces and Abstraction - Exercise/10. Explicit Interfaces/Interfaces/IResident.cs
@@ -0,0 +1,11 @@
+﻿namespace _10._Explicit_Interfaces.Interfaces
+{
+    public interface IResident
+    {
+        string Name { get; }
+
+        string Country { get; }
+
+        string GetName();
+    }
+}
---
+++ 10. Interfaces and Abstraction - Exercise/10. Explicit Interfaces/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace _10._Explicit_Interfaces
+{
+    using Interfaces;
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var citizens = ReadCitizens();
+            PrintNames(citizens);
+        }
+
+        private static void PrintNames(List<Citizen> citizens)
+        {
+            foreach (var citizen in citizens)
+            {
+                Console.WriteLine(((IPerson)citizen).GetName());
+                Console.WriteLine(((IResident)citizen).GetName());
+            }
+        }
+
+        private static List<Citizen> ReadCitizens()
+        {
+            var citizens = new List<Citizen>();
+
+            while (true)
+            {
+                var inputLine = Console.ReadLine();
+
+                if (inputLine == "End")
+                {
+                    break;
+                }
+
+                var citizenInfo = inputLine
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                var name = citizenInfo[0];
+                var country = citizenInfo[1];
+                var age = int.Parse(citizenInfo[2]);
+
+                var citizen = new Citizen(name, country, age);
+                citizens.Add(citizen);
+            }
+
+            return citizens;
+        }
+    }
+}
