df59fe1b7343b941f6add584a6e14e50a065d5e8
Teodor <thelad43@gmail.com>
2018-06-16 12:41:18 +0300

    Added solution for problem 01. Math Operations

---
+++ 11. Polymorphism - Lab/01. Math Operations/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _01._Math_Operations
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var mathOperation = new MathOperation();
+            Console.WriteLine(mathOperation.Add(2, 3));
+            Console.WriteLine(mathOperation.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mathOperation.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
